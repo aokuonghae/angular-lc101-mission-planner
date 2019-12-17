782ed435109f0ad2848bcccb7acbc83221465063
Anastasia Okuonghae <aokuonghae@gmail.com>
2019-12-16 21:41:47 -0600

    studio 2 bonus completed

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipment: string[] = [ "Habitat dome","Drones","Food containers","Oxygen tanks"] 
+  pieceBeingEdited: string= null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(equipmentName:string){
+    this.equipment.push(equipmentName)
+  }
+
+  remove(pieces:string){
+    let index=this.equipment.indexOf(pieces);
+    this.equipment.splice(index,1)
+  }
+
+  edit(pieces:string){
+    this.pieceBeingEdited=pieces;
+  }
+  
+  save(renamedEquipment:string){    
+    return renamedEquipment;
+    this.pieceBeingEdited=null;
+  }
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
+  experiments: string[]=["Mars soil sample","Plant growth in habitat","Human bone density"];
+  testBeingEdited: string=null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(testName:string){
+    this.experiments.push(testName)
+  }
+
+  remove(test:string){
+    let index=this.experiments.indexOf(test);
+    this.experiments.splice(index,1)
+  }
+
+  edit(test:string){
+    this.testBeingEdited=test;
+  }
+  
+  save(newTest:string){    
+    return newTest;
+    this.testBeingEdited=null;
+  }
 }
